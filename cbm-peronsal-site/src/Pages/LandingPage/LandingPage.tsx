a932dc951b99d050319b1c812d8a1f6e318e518b
mbsColin <colin@mbshighway.com>
2024-01-29 15:57:56 -0500

    refined landing page further

--- cbm-peronsal-site/src/Pages/LandingPage/LandingPage.tsx
+++ cbm-peronsal-site/src/Pages/LandingPage/LandingPage.tsx
@@ -1,67 +1,28 @@
-// import { Box, Stack } from '@mui/material';
-// import { useState } from 'react';
-
-// const LandingPage = () => {
-//   const [hoveredBox, setHoveredBox] = useState<number>(0);
-
-//   const handleMouseEnter = (boxIndex: number) => {
-//     setHoveredBox(boxIndex);
-//   };
-
-//   const handleMouseLeave = () => {
-//     setHoveredBox(0);
-//   };
-
-//   return (
-//     <Box style={{ width: '100vw', height: '100vh', backgroundColor: '#222222' }}>
-//       <Stack
-//         direction={'row'}
-//         spacing={20}
-//         justifyContent="center"
-//         alignItems="center"
-//         style={{ height: '100%' }}
-//       >
-//         {[1, 2, 3].map((boxIndex) => (
-//           <Box
-//             key={boxIndex}
-//             onMouseEnter={() => handleMouseEnter(boxIndex)}
-//             onMouseLeave={handleMouseLeave}
-//             style={{
-//               height: '100%',
-//               width: '5%',
-//               backgroundColor: ['#FF0066', '#00FF66', '#0066FF'][boxIndex - 1],
-//               boxShadow: '5px 0 40px #000000, -5px 0 40px #000000',
-//               transition: 'transform 0.3s, opacity 0.3s',
-//               transform: hoveredBox === boxIndex ? 'scaleX(4) translate3d(0, 0, 0)' : 'scaleX(1)',
-//               opacity: hoveredBox === boxIndex ? 0.5 : 1,
-//             }}
-//           ></Box>
-//         ))}
-//       </Stack>
-//     </Box>
-//   );
-// };
-
-// export default LandingPage;
-
-import { Box, Stack } from '@mui/material';
+import { Box, Stack, Typography } from '@mui/material';
 import { useState } from 'react';
+import ArrowBackIosNewRoundedIcon from '@mui/icons-material/ArrowBackIosNewRounded';
+import logo from '../../images/logo.png';
+import Image from 'next/image';
 
 const LandingPage = () => {
-  const [hoveredBox, setHoveredBox] = useState(null);
-
-  const handleMouseEnter = (boxIndex) => {
-    setHoveredBox(boxIndex);
-  };
-
-  const handleMouseLeave = () => {
-    setHoveredBox(null);
-  };
+  const [selectedBox, setSelectedBox] = useState<number>(0);
+  const [hoveredBox, setHoveredBox] = useState<number>(0);
 
   return (
     <Box
-      style={{ width: '100vw', height: '100vh', backgroundColor: '#222222' }}
+      style={{ width: '100vw', height: '100vh', backgroundColor: '#584894' }}
     >
+      <Typography
+        variant="h3"
+        style={{
+          color: 'white',
+          position: 'absolute',
+          transition: 'opacity 0.3s',
+          opacity: !selectedBox ? 1 : 0,
+        }}
+      >
+        <Image src={logo} alt="logo_image" width={200} height={200} />
+      </Typography>
       <Stack
         direction={'row'}
         spacing={20}
@@ -72,24 +33,35 @@ const LandingPage = () => {
         {[1, 2, 3].map((boxIndex) => (
           <Box
             key={boxIndex}
-            onMouseEnter={() => handleMouseEnter(boxIndex)}
-            onMouseLeave={handleMouseLeave}
+            onClick={
+              selectedBox !== boxIndex
+                ? () => setSelectedBox(boxIndex)
+                : undefined
+            }
+            onMouseOver={() => setHoveredBox(boxIndex)}
+            onMouseLeave={() => setHoveredBox(0)}
             style={{
               zIndex: 2,
               position: 'relative',
               height: '100%',
-              width: '5%',
-              backgroundColor: ['#FF0066', '#00FF66', '#0066FF'][boxIndex - 1],
-              boxShadow: '5px 0 40px #000000, -5px 0 40px #000000',
-              transition: 'transform 0.3s, opacity 0.3s',
-              transform: hoveredBox === boxIndex ? 'scaleX(4)' : 'scaleX(1)',
+              width:
+                selectedBox === boxIndex
+                  ? '60%'
+                  : hoveredBox === boxIndex
+                  ? '7.5%'
+                  : '5%',
+              backgroundColor: ['#3afca5', '#01cbae', '#2082a6'][boxIndex - 1],
+              boxShadow: '5px 0 40px #5f2a84, -5px 0 40px #5f2a84',
+              transition: 'width 0.3s, opacity 0.3s',
+              transform: 'scaleX(1)',
               opacity: 1,
               overflow: 'hidden',
+              cursor: selectedBox !== boxIndex ? 'pointer' : 'default',
             }}
           >
-            <div
+            <Box
               style={{
-                zIndex: 0,
+                zIndex: 1,
                 position: 'absolute',
                 top: 0,
                 left: 0,
@@ -97,12 +69,61 @@ const LandingPage = () => {
                 height: '100%',
                 backgroundImage: `url("https://static.vecteezy.com/system/resources/previews/007/632/073/large_2x/dots-mesh-wave-digital-background-illustration-vector.jpg")`,
                 backgroundSize:
-                  hoveredBox === boxIndex ? '500% 100%' : '1000% 100%',
+                  selectedBox === boxIndex ? '200% 100%' : '1000% 100%',
                 backgroundPosition: bgPosition[boxIndex - 1],
-                opacity: hoveredBox === boxIndex ? 0.5 : 0,
+                opacity: selectedBox === boxIndex ? 0.4 : 0,
                 transition: 'opacity 0.3s, background-size 0.3s',
               }}
-            ></div>
+            >
+              {selectedBox === boxIndex ? (
+                <Stack
+                  direction={'column'}
+                  style={{ position: 'absolute', zIndex: 0 }}
+                  spacing={3}
+                >
+                  <Typography
+                    variant="h3"
+                    style={{
+                      color: 'black',
+                      paddingLeft: '24px',
+                      paddingTop: '12px',
+                    }}
+                  >
+                    <ArrowBackIosNewRoundedIcon
+                      style={{ cursor: 'pointer' }}
+                      onClick={() => setSelectedBox(0)}
+                    />
+                  </Typography>
+                </Stack>
+              ) : (
+                <></>
+              )}
+            </Box>
+            {selectedBox === boxIndex ? (
+              <Stack
+                direction={'column'}
+                style={{ position: 'absolute', zIndex: 0 }}
+                spacing={3}
+              >
+                <Box
+                  style={{
+                    zIndex: 0,
+                    position: 'absolute',
+                    top: 0,
+                    left: 0,
+                    width: '100%',
+                    height: '100%',
+                    opacity: selectedBox === boxIndex ? 1 : 0,
+                    transition: 'opacity 0.3s',
+                    color: 'black',
+                    paddingLeft: '48px',
+                    paddingTop: '33.5px',
+                  }}
+                >
+                  Close
+                </Box>
+              </Stack>
+            ) : null}
           </Box>
         ))}
       </Stack>
