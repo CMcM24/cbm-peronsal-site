eadc4d85bedab7feef450345d8fcb8f4f512cf6c
mbsColin <colin@mbshighway.com>
2024-02-13 14:11:32 -0500

    images added

--- cbm-peronsal-site/src/Components/AboutMeSlide/AboutMeSlide.tsx
+++ cbm-peronsal-site/src/Components/AboutMeSlide/AboutMeSlide.tsx
@@ -1,5 +1,7 @@
-import { Box, Typography } from '@mui/material';
+import { Box, Stack, Typography } from '@mui/material';
 import { useEffect, useState } from 'react';
+import prolfilePhoto from '../../images/wedding1.jpg';
+import Image from 'next/image';
 
 const AboutMeSlide = () => {
   const [fadeIn, setFadeIn] = useState(false);
@@ -31,11 +33,53 @@ const AboutMeSlide = () => {
           fontSize: 24,
         }}
       >
-        Full Stack Web Developer with 4 years of Front-End Development
-        experience in a Scrum/Agile development workflow, with a prior
-        background in Sales and Security. Has a passion for work that combines
-        technical know-how with a creative spirit in order to produce effective
-        and easy to use applications.
+        <Stack direction="row">
+          <Typography>
+            <div>
+              <Image
+                src={prolfilePhoto}
+                alt="profile_image"
+                style={{ marginRight: '16px', float: 'left', maxWidth: 200, height: "auto" }}
+              />
+              My name is Colin McMahon. I grew up in central Jersey. In 2010 I
+              set off to college and attended Norwich University up in
+              Northfield, VT. There I tool part in their Corps of Cadet program,
+              where I lived a military barracks lifestyle and got an NROTC
+              education as well as my B.A. in Criminal Justice.
+              <br />
+              <br />
+              After graduation I planned on pursuing a career in law
+              enforcement, but initially ended up working for a company called
+              G4S Secure Solutions in a role as a Site Security Manager for a
+              corporate banking office building. I worked there for two years,
+              until I found myself desiring to find a new challenge. I ended up
+              accepting a job as a Tech Salesman for a company called SHI
+              International. My customer base copmprised of all Texas Local
+              Government accounts. I worked there for another 2 years, selling
+              everything from printer ink cartridges to complete county-wide
+              data center overhauls.
+              <br />
+              <br />I found myself losing interest in the sales part of the job,
+              but found a growing interest in the tech and software itself. I
+              learned that Rutgers University hosted a Full Stack Coding
+              Bootcamp right near my at the time, and I decided to enroll. After
+              6 months of 3hr classes 3 days a week, I completed the bootcamp
+              and got a job as a Front End React Developer. I'm now a Front End
+              Developer with 4 years of experience and looking for a new
+              exciting challenge nad an opportunity to grow and develop as a
+              developer.
+              <br />
+              <br />
+              My professional skills and experience include: React.js, Next.js,
+              TypeScript/JavaScript, Data Visualization (Amcharts), Agile
+              Development, Atlassian Tools (Jira, bitbucket, etc), Giuthub,
+              Git/Gitbash, Figma, Unit Testing (Jest, React Testing Library),
+              Node.js, OOP, Functional Programming in React, Exposure to Azure
+              and AWS Cloud, MySQL, Mentoring, Management, Leadership,
+              Communication
+            </div>
+          </Typography>
+        </Stack>
       </Box>
     </>
   );
